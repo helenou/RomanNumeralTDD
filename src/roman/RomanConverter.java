edb93e0b8b1af63791733f444265eb46f27a9d68
helenou <ln.lejeune@gmail.com>
2015-02-17 21:28:59 +0000

    Change the converter() to pass test for "II". Passed. GREEN

--- src/roman/RomanConverter.java
+++ src/roman/RomanConverter.java
@@ -3,8 +3,14 @@ package roman;
 public class RomanConverter {
 
 	public int convert(String roman) {
-		return 1;
-		//throw new IllegalArgumentException();
+		
+		if("I".equals(roman)){
+			return 1;	
+		} else if("II".equals(roman)){
+			return 2;
+		} else {
+			throw new IllegalArgumentException();
+		}
 	}//end convert()
 
 }//end class
